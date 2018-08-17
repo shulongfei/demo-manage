29262f080d01216e29f2145b1d13411a1fc22aec
舒龙飞 <shulongfei@shulongfeideMacBook-Air.local>
2018-08-17 14:57:38 +0800

    00

--- views/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-	header('Content-Type:text/html; charset=utf-8');
-	header('content-type:application:json;charset=utf8'); 
-	header('Access-Control-Allow-Origin:*'); 
-	header('Access-Control-Allow-Headers:x-requested-with,content-type');
-	$conn = mysqli_connect("127.0.0.1","root","","easyui");
-    $sql = "SET NAMES UTF8";
-	mysqli_query($conn,$sql);
-?>
